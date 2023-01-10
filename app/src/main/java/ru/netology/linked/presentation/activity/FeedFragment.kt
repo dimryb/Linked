1d6b361b0fbd341b42df6327f2f6f97d232be723
dimryb <dimryb@bk.ru>
2023-01-10 22:34:23 +0700

    Пустой класс фрагмента стены

--- app/src/main/java/ru/netology/linked/presentation/activity/FeedFragment.kt
+++ app/src/main/java/ru/netology/linked/presentation/activity/FeedFragment.kt
@@ -1,4 +1,35 @@
 package ru.netology.linked.presentation.activity
 
-class FeedFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+import ru.netology.linked.databinding.FragmentFeedBinding
+
+@AndroidEntryPoint
+class FeedFragment: Fragment() {
+
+    private var _binding: FragmentFeedBinding? = null
+    private val binding: FragmentFeedBinding
+        get() = _binding ?: throw RuntimeException("FragmentFeedBinding == null!")
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFeedBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
